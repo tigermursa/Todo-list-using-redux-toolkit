2393e59f47273ab7017d946047f59486ff781108
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-07 22:47:02 +0600

    "updated the noting list screen"

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -15,26 +15,41 @@ const TodoContainer = () => {
   const { data: todos, isError, isLoading } = useGetTodosQuery(priority);
 
   if (isLoading) {
-    return <div className="flex justify-center items-center h-[70vh] font-semibold text-purple-500 text-2xl lg:text-5xl">Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-[70vh] font-semibold text-purple-500 text-2xl lg:text-5xl">
+        Loading...
+      </div>
+    );
   }
 
   return (
     <div>
-      <div className="flex justify-between mb-5">
-        {/* <Button className="bg-primary-gradient text-xl font-semibold">Add todo</Button> */}
-        <AddTodoModal />
-        <TodoFilter priority={priority} setPriority={setPriority} />
-      </div>
-      <div className="  w-full  rounded-xl   bg-primary-gradient p-1">
-        {/*<div className="bg-white p-5 flex justify-center items-center rounded-md text-2xl text-bold">
+      {todos?.data.length == +0 ? (
+        <div className="h-[70vh] flex flex-col gap-7 justify-center items-center border rounded-full">
+          <p className="  font-semibold text-blue-600 text-2xl lg:text-5xl font-mono">
+            Nothing Todo!😁
+          </p>
+          <AddTodoModal />
+        </div>
+      ) : (
+        <div>
+          <div className="flex justify-between mb-5">
+            {/* <Button className="bg-primary-gradient text-xl font-semibold">Add todo</Button> */}
+            <AddTodoModal />
+            <TodoFilter priority={priority} setPriority={setPriority} />
+          </div>
+          <div className="  w-full  rounded-xl   bg-primary-gradient p-1">
+            {/*<div className="bg-white p-5 flex justify-center items-center rounded-md text-2xl text-bold">
           <p>There is no task pending </p>
   </div>*/}
-        <div className="bg-white p-1 md:p-4 lg:p-5 w-full h-full rounded-lg space-y-4">
-          {todos?.data?.map((item) => (
-            <TodoCard key={item._id} {...item} />
-          ))}
+            <div className="bg-white p-1 md:p-4 lg:p-5 w-full h-full rounded-lg space-y-4">
+              {todos?.data?.map((item) => (
+                <TodoCard key={item._id} {...item} />
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
